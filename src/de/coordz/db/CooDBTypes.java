260fb35948b6ade230b13d6c74ba4d395051a06d
tobias.ohm <tobias.ohm@Ohm-T-3.utintra.com>
2018-05-16 13:30:50 +0200

    - changed comment

--- src/de/coordz/db/CooDBTypes.java
+++ src/de/coordz/db/CooDBTypes.java
@@ -12,7 +12,7 @@ public enum CooDBTypes
 {
 	DERBY("Derby", CooDBDerby.class), 				// Fully working
 	ORACLE("Oracle", CooDBOracle.class),			// Fully working
-	SQLSERVER("SQLServer", CooDBSQLServer.class),	// Not tested yet
+	SQLSERVER("SQLServer", CooDBSQLServer.class),	// Fully working - productive
 	MYSQL("MySQL", CooDBMySQL.class),				// Not tested yet
 	MARIADB("MariaDB", CooDBMariaDB.class);			// Not tested yet
 	
